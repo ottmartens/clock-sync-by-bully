8b8007b7da6e57a2d4495958021dce3d463a1d11
Ott Martens <ott.martens@pipedrive.com>
2021-04-10 19:53:20 +0300

    Set coordinator

--- src/node/routes/internal/newCoordinator.js
+++ src/node/routes/internal/newCoordinator.js
@@ -1,7 +1,10 @@
+const nodeData = require('../../nodeData');
 const { logger } = require('../../../utils');
 
 module.exports = (req, res) => {
 	const newCoordinator = Number(req.body.newCoordinator);
+
+	nodeData.coordinator = newCoordinator;
 
 	logger.debug(`acknowledging ${newCoordinator} as the new coordinator`);
 
